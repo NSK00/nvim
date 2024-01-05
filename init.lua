552d3547ad42c8f00dbb6d716ceaf3801a79aa7f
NSK00 <nolan.hatchell.mcneil@gmail.com>
2024-01-04 19:31:20 -0500

    First commit

---
+++ init.lua
@@ -0,0 +1,60 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " " 
+
+vim.keymap.set('i','jk','<ESC>')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  {"rebelot/kanagawa.nvim", name = "kanagawa" ,  priority = 1000 },
+  {'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = {'nvim-lua/plenary.nvim'} },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {"nvim-neo-tree/neo-tree.nvim", branch = "v3.x" , dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  }},
+  {"williamboman/mason.nvim", name = "mason"}
+}
+
+local opts = {}
+
+--lazy pkg manager
+require("lazy").setup(plugins,opts)
+
+--telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n','<C-p>', builtin.find_files, {})
+vim.keymap.set('n','<leader>fg', builtin.live_grep, {})
+
+--treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"cpp", "c" ,"rust", "javascript", "lua" ,"nim"},
+  highlight = {enable = true },
+  indent = {enable = true},
+})
+
+--neo-tree
+
+--mason
+require("mason").setup()
+
+--for colorscheme
+require("kanagawa").setup()
+vim.cmd 'colorscheme kanagawa-dragon'
